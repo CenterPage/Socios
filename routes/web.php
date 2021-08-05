2d03422a3ac588b05956df334ae435e1596b5d9c
Eddy Work <workingrufino@gmail.com>
2021-08-05 14:57:24 -0500

    index ready

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'auth.login');
 
+Route::get('buscar-socio', 'Search\SearchSocioController')->name('search.socio');
 Route::resource('socios', 'SocioController');
 
-Auth::routes();
+Auth::routes(["register" => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
